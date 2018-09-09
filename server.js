698d8a18f75d5a44c081fa6b3fefadf98d7f380e
Sonya <sonjay08@gmail.com>
2018-09-09 10:26:57 -0500

    deployed with zeit

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(
 );
 
 //CORS
+app.use(cors());
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
